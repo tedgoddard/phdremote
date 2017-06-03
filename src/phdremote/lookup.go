b75de98cc9cf2ed15debd8c9c73be9b4c4b83e09
Ted Goddard <ted.goddard@robotsandpencils.com>
2017-06-03 13:46:57 -0600

    bite sized for raspberry pi

--- src/phdremote/lookup.go
+++ src/phdremote/lookup.go
@@ -7,6 +7,9 @@ func LookupObject(object string) (string, string) {
     if (ok)  {
         return fmt.Sprintf("%f", messier[0]), fmt.Sprintf("%f", messier[1])
     }
+    if (len(NGC) == 0) {
+        setupNGC()
+    }
     ngc, ok := NGC[object]
     if (ok)  {
         return fmt.Sprintf("%f", ngc[0]), fmt.Sprintf("%f", ngc[1])
